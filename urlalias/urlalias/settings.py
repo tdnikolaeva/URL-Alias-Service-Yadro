fe00affe68c6377384f1c27c052957563f75394c
tdnikolaeva <tdnikolaeva@mail.ru>
2025-05-31 16:48:29 +0300

    feat: add .env

--- urlalias/urlalias/settings.py
+++ urlalias/urlalias/settings.py
@@ -1,3 +1,5 @@
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -8,7 +10,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ejxv#nkwq$8)06$6b^2r=+fz8sa=gdg*j^$3m2i5om56l@ks!h'
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-ejxv#nkwq$8)06$6b^2r=+fz8sa=gdg*j^$3m2i5om56l@ks!h')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
