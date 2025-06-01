e5ca78f90d03e6f1e9006da4cdd3ce298b7e728b
tdnikolaeva <tdnikolaeva@mail.ru>
2025-06-01 13:51:01 +0300

    refactor: refactor some files

--- urlalias/api/serializers.py
+++ urlalias/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Link, Usage
+from .models import Link
 
 
 class LinkSerializer(serializers.ModelSerializer):
--- urlalias/api/urls.py
+++ urlalias/api/urls.py
@@ -2,10 +2,12 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import LinkViewSet, UsagesViewSet, ShortLinkViewSet
 
+
 router = DefaultRouter()
 router.register(r'links', LinkViewSet, basename='links')
 router.register(r'statistics', UsagesViewSet, basename='statistics')
 router.register(r'', ShortLinkViewSet, basename='redirect')
+
 
 urlpatterns = [
     path('', include(router.urls)),
